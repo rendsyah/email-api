881a3fa87e8da2e46a8f405463730060c813cbd7
rendsyah <rndyfrdynsyh@gmail.com>
2022-11-06 22:58:23 +0700

    refactor naming

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseConfigModule } from './commons/config/databases/mongoose/mongoose.module';
 import { TypeOrmConfigModule } from './commons/config/databases/typeorm/typeorm.module';
-import { ValidationConfigDatabase } from './commons/config/databases/database.config';
-import { BaseFunctionsModule } from './commons/libs/function.module';
+import { validationConfigSchema } from './commons/config/env.config';
+import { BaseFunctionsModule } from './commons/libs/base-function.module';
 import { EmailModule } from './modules/email/email.module';
 
 @Module({
     imports: [
-        ConfigModule.forRoot({ isGlobal: true, cache: true, validate: ValidationConfigDatabase }),
+        ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true, cache: true, validationSchema: validationConfigSchema }),
         MongooseConfigModule,
         TypeOrmConfigModule,
         BaseFunctionsModule,
