f1426c5a7237051fc264f8e7d0a0e88aefd5b760
rendsyah <rndyfrdynsyh@gmail.com>
2022-11-06 23:02:12 +0700

    refactoring name

--- src/modules/email/email.controller.ts
+++ src/modules/email/email.controller.ts
@@ -1,20 +1,20 @@
 import { Body, Controller, Get, Post, UsePipes } from '@nestjs/common';
 import { JoiValidationPipe } from 'src/commons/pipes/joi.pipe';
 import { EmailNotificationDTO } from './dto/email.dto';
-import { EmailNotificationSchema } from './email.pipe';
+import { emailNotificationSchema } from './email.pipe';
 import { EmailService } from './email.service';
 
 @Controller('/api/email')
 export class EmailController {
     constructor(private emailService: EmailService) {}
 
-    @UsePipes(new JoiValidationPipe(EmailNotificationSchema))
+    @UsePipes(new JoiValidationPipe(emailNotificationSchema))
     @Post()
     async emailNotificationController(@Body() emailNotificationDTO: EmailNotificationDTO) {
         return await this.emailService.emailNotification(emailNotificationDTO);
     }
 
-    @Get()
+    @Get('/bulk')
     async emailBulkNotificationController() {
         return await this.emailService.emailBulkNotification();
     }
