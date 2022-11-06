35e7ca7ac4d9f1756fa88f9de879cccabfced51f
rendsyah <rndyfrdynsyh@gmail.com>
2022-11-06 23:00:22 +0700

    add postgre service & module

---
+++ src/datasource/postgre/postgre.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { IFindOneEvent } from './interfaces/event';
+import { IFindAllNotificationMessage } from './interfaces/notification-message';
+import { IFindOneTransaction } from './interfaces/transaction';
+import { NotificationMessage } from './models/notification-message.entity';
+
+@Injectable()
+export class PostgreService {
+    constructor(@InjectRepository(NotificationMessage) private readonly notificationMessageModel: Repository<NotificationMessage>) {}
+
+    public async findAllNotificationMessage(params: IFindAllNotificationMessage) {
+        const { mediaId, status } = params;
+        return await this.notificationMessageModel.find({ where: { media_id: mediaId, status: status } });
+    }
+
+    public async findOneEvent(params: IFindOneEvent) {
+        return;
+    }
+
+    public async findOneTransaction(params: IFindOneTransaction) {
+        return;
+    }
+}
