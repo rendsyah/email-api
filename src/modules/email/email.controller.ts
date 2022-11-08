1f8949ec7fc45d2c5ba60a9dbd0e3855d4bfbaa9
rendsyah <rndyfrdynsyh@gmail.com>
2022-11-09 04:42:23 +0700

    add api documentation

--- src/modules/email/email.controller.ts
+++ src/modules/email/email.controller.ts
@@ -1,21 +1,32 @@
 import { Body, Controller, Get, Post, UsePipes } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { JoiValidationPipe } from 'src/commons/pipes/joi.pipe';
-import { EmailNotificationDTO } from './dto/email.dto';
-import { emailNotificationSchema } from './email.pipe';
+import { ApiEmailBadRequest, ApiEmailBody, ApiEmailCreated, ApiEmailInternalServerError, ApiEmailOperation } from './api-docs/email.docs';
+import { EmailBulkNotificationDTO, EmailNotificationDTO } from './dto/email.dto';
+import { emailBulkNotificationSchema, emailNotificationSchema } from './email.pipe';
 import { EmailService } from './email.service';
 
+@ApiTags('Email')
 @Controller('/api/email')
 export class EmailController {
     constructor(private emailService: EmailService) {}
 
     @UsePipes(new JoiValidationPipe(emailNotificationSchema))
     @Post()
+    @ApiEmailOperation()
+    @ApiEmailBody(EmailNotificationDTO)
+    @ApiEmailCreated(EmailNotificationDTO)
+    @ApiEmailBadRequest(EmailNotificationDTO)
+    @ApiEmailInternalServerError(EmailNotificationDTO)
     async emailNotificationController(@Body() emailNotificationDTO: EmailNotificationDTO) {
         return await this.emailService.emailNotification(emailNotificationDTO);
     }
 
+    @UsePipes(new JoiValidationPipe(emailBulkNotificationSchema))
     @Get('/bulk')
+    @ApiEmailOperation('bulk')
+    @ApiEmailBody(EmailBulkNotificationDTO, 'bulk')
     async emailBulkNotificationController() {
-        return await this.emailService.emailBulkNotification();
+        return await this.emailService.emailBulkNotification(EmailBulkNotificationDTO);
     }
 }
